eabac68f25a1962be6b31f51a2d475563e7652cf
srinivas-reddy-a <it@arraykart.com>
2021-12-30 16:47:53 +0530

    database setup

--- config/database.js
+++ config/database.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 const db = knex({
       client: 'pg',
-      connection: process.env.DATABASE_URL || {
+      connection: {
         host : process.env.DB_HOST || '127.0.0.1',
         user : process.env.DB_USER || 'postgres',
         password : process.env.DB_PASS || 'postgres',
