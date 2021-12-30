ade5bf3397fd070c75bb43869cbe114c991c89bb
srinivas-reddy-a <it@arraykart.com>
2021-12-30 16:57:25 +0530

    database setup

--- config/database.js
+++ config/database.js
@@ -6,10 +6,8 @@ dotenv.config();
 const db = knex({
       client: 'pg',
       connection: {
-        host : process.env.DB_HOST || '127.0.0.1',
-        user : process.env.DB_USER || 'postgres',
-        password : process.env.DB_PASS || 'postgres',
-        database : process.env.DB_NAME || 'farmer-survey'
+        connectionString: process.env.DATABASE_URL,
+        ssl: { rejectUnauthorized: false }
       },
   });
 
